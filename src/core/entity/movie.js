785612640a2a2a015708173942e3cf36dadb1f41
MaidedPonce <maided.h.p90@gmail.com>
2022-12-01 19:45:29 -0600

    adding more directories and one route

---
+++ src/core/entity/movie.js
@@ -0,0 +1,9 @@
+class Movie {
+    constructor(title, description, originalTitle, clasification, duration) {
+        this.title = title,
+        this.description = description,
+        this.originalTitle = originalTitle,
+        this.clasification = clasification,
+        this.duration = duration
+    }
+}
