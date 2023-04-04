6832fda3ffc6e9fff7035baf425c9cedb931766f
MaidedPonce <maided.h.p90@gmail.com>
2023-04-04 00:02:30 -0500

    initializing documentation to the project

--- src/app.js
+++ src/app.js
@@ -7,18 +7,41 @@ const API_PREFIX = process.env.API_PREFIX || '/api/v1'
 const depedencies = require('./config/dependencies')
 const ErrorHandler = require('./frameworks/expressSpecific/ErrorHandler')
 const { connect: connectToMongo } = require('./frameworks/db/mongo')
+const path = require('path')
+// swagger
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerSpec = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Ca cartelera is an api made it with Mongo, Express and Node',
+      version: '1.0.0',
+      servers: [
+        {
+          url: 'http://localhost:3000'
+        }
+      ]
+    }
+  },
+  apis: [`${path.join(__dirname, './frameworks/expressSpecific/routes/*.js')}`]
+}
 
 module.exports = {
-    start: () => {
-        app.use(express.json())
-        app.use(express.urlencoded({
-            extended: true
-        }))
-        app.use(API_PREFIX, routes(depedencies))
-        app.use(ErrorHandler)
-        app.listen(PORT, () => {
-            console.log(`App is running under port ${PORT}`)
-            connectToMongo()
-        })
-    }
+  start: () => {
+    app.use(express.json())
+    app.use(
+      express.urlencoded({
+        extended: true
+      })
+    )
+    app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
+
+    app.use(API_PREFIX, routes(depedencies))
+    app.use(ErrorHandler)
+    app.listen(PORT, () => {
+      console.log(`App is running under port ${PORT}`)
+      connectToMongo()
+    })
+  }
 }
--- src/frameworks/expressSpecific/routes/movies.js
+++ src/frameworks/expressSpecific/routes/movies.js
@@ -3,11 +3,64 @@ const express = require('express')
 const { movieControllers } = require('../../../controllers')
 
 module.exports = dependencies => {
-    const router = express.Router()
-    const { getMovieById, getAllMovies, addMovies } = movieControllers(dependencies)
+  const router = express.Router()
+  const { getMovieById, getAllMovies, addMovies } =
+    movieControllers(dependencies)
 
-    router.route('/').get(getAllMovies)
-    router.route('/:id').get(getMovieById)
-    router.route('/').post(addMovies)
-    return router
+  /**
+   * @swagger
+   *  components:
+   *   schemas:
+   *      Movie:
+   *          type: object
+   *          properties:
+   *              title:
+   *                  type: string
+   *                  description: Title of the movie
+   *              description:
+   *                  type: string
+   *                  description: Description of the movie
+   *              originalTitle:
+   *                  type: string
+   *                  description: Original title of the movie
+   *              clasification:
+   *                  type: string
+   *                  description: Clasification of the movie
+   *              duration:
+   *                  type: string
+   *                  description: Duration of the movie
+   *          required:
+   *              - title
+   *              - description
+   *              - originalTitle
+   *              - clasification
+   *              - duration
+   *          example:
+   *              title: Crepusculo
+   *              description: 'Bella Swan se va a vivir con su padre, al pueblo de Forks, donde conoce a Edward, un atractivo y misterioso chico con un gran secreto: es un vampiro. Pero la familia del chico guarda una peculiaridad: no se alimenta de sangre humana.'
+   *              originalTitle: Crepusculo
+   *              clasification: Romance/Fantasía
+   *              duration: 2h 2m
+   */
+
+  /**
+   * @swagger
+   * /api/v1/movies:
+   * get:
+   *   summary: return all movies
+   *   tags: [Movie]
+   *   responses:
+   *       200:
+   *           description: all movies
+   *           content:
+   *               application/json:
+   *                   schema:
+   *                      type: array
+   *                      items:
+   *                       $ref: '#/components/schemasMovie'
+   */
+  router.route('/').get(getAllMovies)
+  router.route('/:id').get(getMovieById)
+  router.route('/').post(addMovies)
+  return router
 }
